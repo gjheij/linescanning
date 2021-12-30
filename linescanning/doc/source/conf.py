3b54163c05e6177c706fc1f846a07a8370e947a4
Jurjen Heij <j.heij@spinozacentre.nl>
2021-12-30 17:10:22 +0100

    fMRIprep-like docs

--- linescanning/doc/source/conf.py
+++ linescanning/doc/source/conf.py
@@ -77,7 +77,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -91,21 +91,21 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {
-    'github_banner': True,
-    'github_user': 'spinoza-centre',
-    'github_repo': 'linescanning',
-    'github_button': 'true',
-    'description': 'package for (pre-)processing of anatomical and (linescanning) fMRI data',
-    'font_size': 9,
-    'font_family': 'roboto, helvetica'
-}
+# html_theme_options = {
+#     'github_banner': True,
+#     'github_user': 'spinoza-centre',
+#     'github_repo': 'linescanning',
+#     'github_button': 'true',
+#     'description': 'package for (pre-)processing of anatomical and (linescanning) fMRI data',
+#     'font_size': 9,
+#     'font_family': 'roboto, helvetica'
+# }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -117,13 +117,13 @@ html_static_path = ['../_static']
 #
 # This is required for the alabaster theme
 # refs: http://alabaster.readthedocs.io/en/latest/installation.html#sidebars
-html_sidebars = {
-    '**': [
-        'relations.html',  # needs 'show_related': True theme option to display
-        'navigation.html',
-        'searchbox.html',
-    ]
-}
+# html_sidebars = {
+#     '**': [
+#         'relations.html',  # needs 'show_related': True theme option to display
+#         'navigation.html',
+#         'searchbox.html',
+#     ]
+# }
 
 
 # -- Options for HTMLHelp output ------------------------------------------
@@ -134,23 +134,23 @@ htmlhelp_basename = 'linescanningdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    'papersize': 'A4paper',
+# latex_elements = {
+#     # The paper size ('letterpaper' or 'a4paper').
+#     #
+#     'papersize': 'A4paper',
 
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    'pointsize': '10pt',
+#     # The font size ('10pt', '11pt' or '12pt').
+#     #
+#     'pointsize': '10pt',
 
-    # Additional stuff for the LaTeX preamble.
-    #
-    'preamble': '',
+#     # Additional stuff for the LaTeX preamble.
+#     #
+#     'preamble': '',
 
-    # Latex figure (float) alignment
-    #
-    'figure_align': 'htbp',
-}
+#     # Latex figure (float) alignment
+#     #
+#     'figure_align': 'htbp',
+# }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
--- linescanning/doc/source/installation.rst
+++ linescanning/doc/source/installation.rst
@@ -1,8 +1,66 @@
-Installing linescanning
+.. include:: links.rst
+
+------------
+Installation
+------------
+You can install *LineScanning* within a `Manually Prepared Environment (Python 3.7+)`_.
+The workflow to install *LineScanning* is as follows:
+
+* Download the repository from `github`:
+::
+   $ git clone https://github.com/gjheij/linescanning.git
+
+* Setup the environment for `bash`.
+::
+
+  $ bash linescanning/linescanning/shell/spinoza_setup setup   
+
+* This will print out a set of instructions that are executed under the hood; 
+it makes sure the setup file is loaded in your `.bash_profile` each time you start a new a new terminal
+and makes the scripts inside the repository executable similar to how *FSL* works.
+
+* Setup the `python` environment. Edit the *environment.yml* file to your liking (e.g., paths and environment name) and 
+execute the following commands:
+::
+   $ cd linescanning
+   $ conda create --name <environment name> --file environment.yml
+   $ pip install -e .
+
+* Test the installation with:
+::
+   $ python -c "import linescanning"
+
+* If no error was given, the installation was successful. To test the `bash` environment, enter the following:
+::
+   $ master
+
+* This should give the help menu of the master script. If not, something went wrong. A first check if to make sure the
+files in `linescanning/bin` and `linescanning/shell` are indeed executables. If not, hit the following and try again:
+::
+   $ chmod -R 775 linescanning/bin
+   $ chmod -R 775 linescanning/shell
+
+Manually Prepared Environment (Python 3.7+)
 ===========================================
 
-From Github
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Right now you can clone the main branch of nideconv using git ::
+Make sure all of *LineScanning*'s `External Dependencies`_ are installed.
+These tools must be installed and their binaries available in the
+system's ``$PATH``.
+As an additional installation setting, FreeSurfer requires a license file (see `<https://surfer.nmr.mgh.harvard.edu/fswiki/License>`).
 
-   pip install git+https://github.com/gjheij/linescanning.git
+External Dependencies
+---------------------
+*LineScanning* is written using Python 3.7 (or above). *LineScanning* requires some other neuroimaging software tools that are
+not handled by the Python's packaging system (Pypi) used to deploy
+the ``LineScanning`` package:
+
+- FSL_ (version 5.0.9)
+- ANTs_ (version 2.3.1)
+- ITK-Snap_ (version 3.8.0)
+- `C3D <https://sourceforge.net/projects/c3d/>`_ (version 1.0.0)
+- FreeSurfer_ (version 7.2.0)
+- Nideconv_
+- pRFpy_
+- CAT12_ (version r1113)
+- SPM_
+- Nighres_
