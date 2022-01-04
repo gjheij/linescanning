1a4eecf877abcdfa2b2d06b35a6e05aff44b85a1
Jurjen Heij <j.heij@spinozacentre.nl>
2022-01-04 18:02:31 +0100

    add more mock

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -188,6 +188,6 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-MOCK_MODULES = ['pycortex', 'nitime', 'nitime.timeseries', 'nitime.analysis', 'prfpy']
+MOCK_MODULES = ['pycortex', 'nitime', 'nitime.timeseries', 'nitime.analysis', 'prfpy', 'seaborn']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
