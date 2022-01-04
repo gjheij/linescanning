a877ab51bd354a7b08707942d8865eac24786863
Jurjen Heij <j.heij@spinozacentre.nl>
2022-01-04 17:25:46 +0100

    move mkdir command to conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,8 +20,8 @@
 import mock
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('../'))
-
+sys.path.insert(0, os.path.abspath('../'))
+os.makedirs(os.path.join(os.path.expanduser('~'), ".config"))
 
 # -- General configuration ------------------------------------------------
 
