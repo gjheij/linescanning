eb992ac3e31b1dc1480b1a92d1b62ce176403895
Jurjen Heij <j.heij@spinozacentre.nl>
2022-01-05 20:44:47 +0100

    accidentally remove nbsphinx extension..

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -40,7 +40,8 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.ifconfig',
               'sphinx.ext.napoleon',
               'sphinx.ext.viewcode',
-              'sphinx.ext.githubpages']
+              'sphinx.ext.githubpages',
+              'nbsphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
