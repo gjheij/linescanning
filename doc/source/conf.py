8cf9b5b0d7165c322dc74d7ad73f8cb43eab121b
gjheij <j.heij@spinozacentre.nl>
2023-03-01 10:48:31 +0100

    scikit-image to mock

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -202,7 +202,8 @@ MOCK_MODULES = [
     'hedfpy', 
     'nideconv',
     'niworkflows',
-    'niworkflows.reports']
+    'niworkflows.reports',
+    'scikit-image']
                 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
